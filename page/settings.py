e70e53fdae31df38936495e894eed7ac10db1b33
VaderX <shuklapriyanshu9@gmail.com>
2020-06-26 14:18:16 +0530

    Update settings.py

--- page/settings.py
+++ page/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'c)xb!chzno+=bwe^^2p%1a3w7p2srla&4hfrprr*0^bx@#s@8l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://kuma9.herokuapp.com/']
 
 
 # Application definition
