a8f59b1b4683aa370d0e771403930240b72ecd36
VaderX <shuklapriyanshu9@gmail.com>
2020-07-01 07:46:19 +0530

    error solving

--- register/models.py
+++ register/models.py
@@ -5,6 +5,6 @@ class User(models.Model):
     mName = models.CharField(max_length = 20)
     mUsername = models.CharField(max_length = 20, primary_key=True)
     mEmail = models.CharField(max_length = 30)
-    mPhone = models.IntegerField(default = 10)
+    mPhone = models.CharField(max_length = 10)
     mPassword = models.CharField(max_length = 30)
 
